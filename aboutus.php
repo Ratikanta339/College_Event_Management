a2c227503c80be9041a165b8a5178cc6a4cb56c7
Ratikanta Sahoo <ratikanta339@gmail.com>
2024-07-21 20:10:06 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Sanchanala 2k20</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 10px solid #00004d;
+  border-radius: 5px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+<body style="background-color:#b3e0ff ">
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p id="bj"> 
+  Established in 1959 at the backdrop of a picturesque landscape, Dhenkanal Autonomous College has carved a niche for itself in academic excellence not only in the state but in the country as well. The results of the students testify to this honour. However, with an endless pursuit of excellence as its motto, the College is forever making an endeavour to excel itself. To achieve this, it emphasizes strategic planning that assesses its current functioning and the modus operandi of executing a higher mission that includes such crucial aspects as financial planning, budgeting, staffing, and academic programming, which, in turn, would essentially relate to matching institutional commitments and the real, concrete benefits to the students. After getting Autonomous status in 2002 the college intended to materialize many plans to enhance the quality of the institution. Its primary mission is to lay out the utmost emphasis on academic excellence, research, innovation, and other extension activities to reach the pinnacle of glory. It aims at effecting radical changes in the syllabi of all the programmes and pedagogy. In this respect, the college is a pioneer in adopting the Choice Based Credit System syllabus in 2016. The CBCS courses for all programmes have been designed based on UGC guidelines. Though the courses are mainly influenced by the model syllabus prescribed by Utkal University, many departments have modified the courses up to a certain extent to cater to the needs of local requirements. In these years, several steps have been taken for examination reforms, automation of the library, online management of accounts, e-administration, e-admission, and the upgrade of Management Information Systems. Due to these initiatives, the college is now a leading feeder college for institutes of national repute like the IITs, IIMs, Central Universities, and other State Universities. Many employers are now being attracted to our institutions because of the requirement for quality human resources in terms of skill, knowledge, and values. The institute provides equal importance to develop a research culture among its students and teachers. As regards the improvement of academic excellence of teachers, they are encouraged to participate in various FDPs, seminars, conferences, refreshers, and orientation courses. For the all-around development of the students, the college offers various extension activities like NCC, NSS, YRC, Rover & Ranger, Sports facilities, Dramatic and other Cultural Activities. The institution aims at developing intellectually fit, environmentally conscious, and socially committed citizens. By fostering a culture of values, critical thinking, reaffirming human rights, and promoting gender justice, the college strives to create a learner-friendly, progressive, and democratic environment. The college has a robust grievance redressal mechanism for all its stakeholders. This pantheon of wisdom and coliseum of learning, situated in the sylvan setting of forests, hills, and mango groves has promised to achieve the goals and objectives in every aspect/field with the untiring efforts of its teaching and supporting staff. The mission and vision of this premier institute are embedded in the crest of the college.
+</p>
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
---
+++ utils/footer.php
@@ -0,0 +1,39 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>D</strong><span class = "small footerSubtext">henkanal</span>
+                        <strong>A</strong><span class = "small footerSubtext">utonomous</span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege,</span>
+                        <strong>D</strong><span class = "small footerSubtext">henkanal</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Our college Mission is to impart quality technical education and higher moral ethics associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future with full confidence
+                  .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address: JJR8+G36, College Rd, College Chowk, Kunjakanta, Dhenkanal, Odisha 759001
+                    
+                     </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                    <a href=""><img src = "images/facebook.png"></a>
+                    <a href=""><img src = "images/instagram.png" width="65px"></a>
+                        
+                        <a href=""><img src = "images/youtube.png"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
